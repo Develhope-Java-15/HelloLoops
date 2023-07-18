329891b6f6f9cdb1536cde1d1abe3e4b38a1de98
aegroto <aegroto@protonmail.com>
2023-07-18 14:39:33 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        /*int i = 0;
+        while(i < 10) {
+            System.out.println("Running...");
+            i++;
+        }*/
+
+        String[] array = {"emanuele", "giuseppe", "maurizio"};
+        for(String element : array) {
+            System.out.println("Array element: " + element);
+        }
+    }
+}
